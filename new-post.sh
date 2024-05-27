7a04df011e5a4c363459924379d5c7293f83f30a
Nic Lake <niclake13@gmail.com>
2024-05-27 13:02:49 -0500

    Update script to auto-name & auto-open the created post

--- new-post.sh
+++ new-post.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+rawTitle=$1
 title=$(echo "$1" | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 cp ~/github/niclake.github.io/_posts/_PostTemplate.md ~/github/niclake.github.io/_posts/$(date +%Y-%m-%d)-$title.md
+sed -i -e "s/TITLE/$rawTitle/g" ~/github/niclake.github.io/_posts/$(date +%Y-%m-%d)-$title.md
+code ~/github/niclake.github.io/_posts/$(date +%Y-%m-%d)-$title.md
