d875ed533cb3b0d326e3459fbd3e53a6bd49a779
Nic Lake <niclake13@gmail.com>
2026-01-05 15:51:57 -0600

    Cleaning

--- src/_data/catalog/books.js
+++ src/_data/catalog/books.js
@@ -1,4 +1,4 @@
-import currentlyReading from'./currentlyReading.json' with { 'type': 'json' }
+import currentlyReading from './currentlyReading.json' with { 'type': 'json' }
 import allBooks from'./allReadBooks.json' with { 'type': 'json' }
 import favoriteBooks from'./favoriteBooks.json' with { 'type': 'json' }
 import readingGoals from'./readingGoals.json' with { 'type': 'json' }
@@ -8,12 +8,8 @@ export default async function () {
 	const favorites = favoriteBooks
 	const goals = readingGoals
 	const datedBooks = allBooks
-		.filter(
-			(book) => book.lastRead !== null
-		)
-		.sort((a, b) => {
-			return a.lastRead > b.lastRead ? -1 : 1;
-		});
+		.filter((book) => book.lastRead !== null)
+		.sort((a, b) => {return a.lastRead > b.lastRead ? -1 : 1;});
 
 	// generate an array of books with yearRead as 'undated'
 	const undatedBooks = allBooks.filter(
@@ -22,7 +18,7 @@ export default async function () {
 
 	// Extract unique years and format them
 	const uniqueYears = [
-		...new Set(datedBooks.map((book) => book.lastRead.split("-")[0])),
+		...new Set(datedBooks.map((book) => book.lastRead.split("-")[0]))
 	]
 		.map((year) => `y${year}`)
 		.sort((a, b) => b.slice(1) - a.slice(1)); // Sort in descending order
